6c08e8e1b7a18b7481616022f89e3b9c33a4eee5
PacoOnix <fhernandez@onixfinanciera.mx>
2024-07-03 06:59:45 -0600

    Integracion de Login

--- index.php
+++ index.php
@@ -1,3 +1,100 @@
-<?php 
-echo "Hola mundo 222";
- ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <title>La Costeña</title>
+        <meta content="Admin Dashboard" name="description" />
+        <meta content="themesdesign" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <link rel="shortcut icon" href="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/images/favicon.ico">
+
+        <link href="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/plugins/animate/animate.css" rel="stylesheet" type="text/css">
+        <link href="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/css/icons.css" rel="stylesheet" type="text/css">
+        <link href="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/css/style.css" rel="stylesheet" type="text/css">
+
+    </head>
+
+
+    <body class="fixed-left">
+        <!-- Begin page -->
+        <!--<div class="accountbg"></div>-->
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div class="wrapper-page">
+
+            <div class="card">
+                <div class="card-body">
+
+                    <h3 class="text-center mt-0">
+                        <a href="index.html" class="logo logo-admin"><img src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/images/logo.png" height="20" alt="logo"></a>
+                    </h3>
+
+                    <h6 class="text-center">Iniciar Sesion</h6>
+
+                    <div class="p-3">
+                        
+                        <form class="form-horizontal" action="index.html">
+
+                            <div class="form-group row">
+                                <div class="col-12">
+                                    <input class="form-control" type="text" required="" placeholder="Username">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-12">
+                                    <input class="form-control" type="password" required="" placeholder="Password">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-12">
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                        <label class="custom-control-label" for="customCheck1">Remember me</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group text-center row m-t-20">
+                                <div class="col-12">
+                                    <button class="btn btn-danger btn-block waves-effect waves-light" type="submit">Log In</button>
+                                </div>
+                            </div>
+
+                            <div class="form-group m-t-10 mb-0 row">
+                                <div class="col-sm-7 m-t-20">
+                                    <a href="pages-recoverpw.html" class="text-muted"><i class="mdi mdi-lock"></i> Forgot your password ?</a>
+                                </div>
+                                <div class="col-sm-5 m-t-20">
+                                    <a href="pages-register.html" class="text-muted"><i class="mdi mdi-account-circle"></i> Create an account ?</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- jQuery  -->
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/jquery.min.js"></script>
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/modernizr.min.js"></script>
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/detect.js"></script>
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/fastclick.js"></script>
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/jquery.slimscroll.js"></script>
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/jquery.blockUI.js"></script>
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/waves.js"></script>
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/jquery.nicescroll.js"></script>
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/jquery.scrollTo.min.js"></script>
+
+        <!-- App js -->
+        <script src="blupper_templete/Dashor_v1.1.0/Admin/vertical/assets/js/app.js"></script>
+
+    </body>
+</html>
